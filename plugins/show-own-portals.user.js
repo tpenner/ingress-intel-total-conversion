e5c3aa1412cb661cba86c83829661ba78df970ff
Tim <tpenner@gmail.com>
2013-02-23 19:23:37 -0800

    avoid null mods and resos

--- plugins/show-own-portals.user.js
+++ plugins/show-own-portals.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @id             iitc-plugin-show-own-portals@tpenner
 // @name           iitc: show own portals
-// @version        0.2
+// @version        0.2.8
 // @namespace      https://github.com/breunigs/ingress-intel-total-conversion
 // @updateURL      https://raw.github.com/breunigs/ingress-intel-total-conversion/gh-pages/plugins/show-own-portals.user.js
 // @downloadURL    https://raw.github.com/breunigs/ingress-intel-total-conversion/gh-pages/plugins/show-own-portals.user.js
-// @description    Uses the fill color of the portals to denote if the portal is owned by you (captured, resonator, shied)
+// @description    Uses the color of the portals to denote if the portal is owned by you (captured, resonator, shield)
 // @include        http://www.ingress.com/intel*
 // @match          http://www.ingress.com/intel*
 // ==/UserScript==
@@ -30,35 +30,40 @@ window.plugin.showOwnPortals.portalAdded = function(data) {
   var portal_owned_by_me = false;
   var myTeam = TEAM_NONE;
   
-  if(PLAYER.team == 'RESISTANCE'){
+  if(PLAYER.team === 'RESISTANCE'){
     var myTeam = TEAM_RES;
-  } else if(PLAYER.team == 'ENLIGHTENED') {
+  } else if(PLAYER.team === 'ENLIGHTENED') {
     var myTeam = TEAM_ENL;
   }
 
   var portalTeam = getTeam(d);
 
-  if((portalTeam != 0) && (portalTeam == myTeam)) {
+  if((portalTeam !== 0) && (portalTeam === myTeam)) {
     //check if portal is captured by me
     if(getPlayerName(d.captured.capturingPlayerId) == PLAYER.nickname) {
         portal_owned_by_me = true;
     }
     //check if any resonator is deployed by me
     $.each(d.resonatorArray.resonators, function(ind, reso) {
-      if(getPlayerName(reso.ownerGuid) == PLAYER.nickname) {
-        portal_owned_by_me = true;
-      }
+	  if(reso !== null) {
+        if(getPlayerName(reso.ownerGuid) === PLAYER.nickname) {
+          portal_owned_by_me = true;
+        }
+	  }
     });      
     //check if any shield is deployed by me
     $.each(d.portalV2.linkedModArray, function(ind, mod) {
-      if(getPlayerName(mod.installingUser) == PLAYER.nickname) {
-        portal_owned_by_me = true;
+	  if(mod !== null) {
+        if(getPlayerName(mod.installingUser) === PLAYER.nickname) {
+          portal_owned_by_me = true;
+        }
       }
     });    
     if(portal_owned_by_me) {
       var color = window.COLOR_OWN_PORTAL;
       var fill_opacity = 1;
-      var params = {fillColor: color, fillOpacity: fill_opacity, radius: data.portal.options.radius+1};
+      var params = {fillColor: color, fillOpacity: fill_opacity};
+//      var params = {fillColor: color, fillOpacity: fill_opacity, radius: data.portal.options.radius+1};
       data.portal.setStyle(params);
     }
   }
